85c7fbab5c5663f185ca1c246477c240097749c1
Oleksandr Kalita <olexadr.kalita@gmail.com>
2023-09-26 14:43:04 +0200

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Java Lab 2 ");
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+
+        do{
+            System.out.println("Switch exercise - '1' '2' '3'");
+            String str = scanner.nextLine();
+
+            switch(str){
+                case "1":
+                case "2":
+                case "3":
+                default:
+                    break;
+            }
+        }while(true);
 
     }
 }
